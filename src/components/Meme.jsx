98d84e9e3be08643bbc4ee63a8873ea7c96c672e
Nassy Kova <mailtosummer@gmail.com>
2023-07-12 12:05:43 +1000

    add meme section WIP

---
+++ src/components/Meme.jsx
@@ -0,0 +1,12 @@
+export default function Meme() {
+    return (
+        <div className="meme">
+            <input className="meme--input" placeholder="type here"></input>
+            <input className="meme--input" placeholder="and here"></input>
+            <button className="meme--button" >
+                Get a new image🖼️
+            </button>
+            Meme
+        </div>
+    );
+}
