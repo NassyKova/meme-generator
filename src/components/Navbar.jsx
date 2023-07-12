7ae04f704da9c9a44362c8260510f2d840b4e2d7
Nassy Kova <mailtosummer@gmail.com>
2023-07-12 10:47:49 +1000

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <img
+                className="nav--logo"
+                src="./img/Trollface.png"
+                alt="trollface"
+            />
+            <h2 className="nav--title">Meme Generator</h2>
+            <section className="nav--title2">Explore the memes</section>
+        </nav>
+    );
+}
